12bd86201a276f32dff9a43d678a5eae4f612bc0
storeline <al221910311@gmail.com>
2025-09-19 11:50:18 -0600

    chacando colores de fondo

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,3 +21,53 @@
       cvButton.classList.remove('visible');
     }
   });
+
+const proyectos = [
+  {
+    titulo: "Slide Core",
+    descripcion: "(PWA) con Angular, Nest.js",
+    imagen: "assets/img/projects/slidecore.jpeg",
+    github: "#",
+    demo: "#"
+  },
+  {
+    titulo: "Portafolio Creativo",
+    descripcion: "HTML, CSS, animaciones personalizadas",
+    imagen: "assets/img/projects/portfolio.jpeg",
+    github: "#",
+    demo: "#"
+  },
+
+    {
+    titulo: "Portafolio Creativo",
+    descripcion: "HTML, CSS, animaciones personalizadas",
+    imagen: "assets/img/projects/portfolio.jpeg",
+    github: "#",
+    demo: "#"
+  },
+  // Agrega más proyectos aquí
+];
+
+const container = document.getElementById("projectsContainer");
+
+proyectos.forEach(proyecto => {
+  const card = document.createElement("div");
+  card.classList.add("card_project");
+
+  card.innerHTML = `
+    <div class="project_wrap">
+      <img src="${proyecto.imagen}" alt="${proyecto.titulo}">
+      <div class="info">
+        <h4>${proyecto.titulo}</h4>
+        <p>${proyecto.descripcion}</p>
+        <div class="links_project">
+          <a href="${proyecto.github}" aria-label="Ver código en GitHub"><i class="bi bi-github"></i></a>
+          <a href="${proyecto.demo}" aria-label="Ver proyecto"><i class="bi bi-eye"></i></a>
+        </div>
+      </div>
+    </div>
+  `;
+  
+  container.appendChild(card);
+});
+
