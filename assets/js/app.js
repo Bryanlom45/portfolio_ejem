c82394619e36e85b39d0e72570ded8b6734de4e8
storeline <al221910311@gmail.com>
2025-10-04 00:49:35 -0600

    corregir errore

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,5 @@
 
 import { proyectos } from './data-proyectos.js';
-console.log("✅ proyectos cargados:", proyectos);
 import { renderProyectos } from './render-proyectos.js';
 import { activarFiltros } from './filtro-proyectos.js';
 import { controlarBotonCV } from './cv-floating.js';
